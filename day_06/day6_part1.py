997e82099c3d86321191f43159222bb20b032d3f
Leander <leander.sabel@gmail.com>
2022-12-06 19:28:58 +0100

    Exchange dictionary for set and fix comments

--- day_06/day6_part1.py
+++ day_06/day6_part1.py
@@ -8,11 +8,11 @@ def main():
     with open('input') as input_streams:
         for input_stream in input_streams:
             for i in range(len(input_stream) - 3):
-                # Create a dictionary from the keys, because it does not keep duplicate values
-                dic = dict.fromkeys([input_stream[i], input_stream[i + 1], input_stream[i + 2], input_stream[i + 3]])
+                # Create a set from the keys, because it does not keep duplicate values
+                s = {input_stream[i], input_stream[i + 1], input_stream[i + 2], input_stream[i + 3]}
 
-                # So if the length of the dictionary is still 4, they are all different
-                if len(dic) == 4:
+                # So if the length of the set is still 4, they are all different
+                if len(s) == 4:
                     # +4 because the target value is the first index after the marker
                     print(i+4)
                     break
--- day_06/day6_part2.py
+++ day_06/day6_part2.py
@@ -8,16 +8,13 @@ def main():
     with open('input') as input_streams:
         for input_stream in input_streams:
             for i in range(len(input_stream) - 13):
-                # Create a dictionary from the keys, because it does not keep duplicate values
-                s = []
+                # Create a set from the keys, because it does not keep duplicate values
+                s = set()
                 for j in range(14):
-                    s.append(input_stream[i+j])
+                    s.add(input_stream[i+j])
 
-                dic = dict.fromkeys(s)
-                #print(len(dic))
-
-                # So if the length of the dictionary is still 4, they are all different
-                if len(dic) == 14:
+                # So if the length of the set is still 14, they are all different
+                if len(s) == 14:
                     # +14 because the target value is the first index after the marker
                     print(i+14)
                     break
